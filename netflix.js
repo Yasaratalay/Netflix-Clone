f08633788603572524aec7eb43d1bd73ea196f42
YasarAtalay <yasar.atalay@santsg.com>
2022-02-21 15:41:17 +0300

    Input change.

--- netflix.js
+++ netflix.js
@@ -5,8 +5,8 @@ window.addEventListener("scroll", scrollActive);
 // Scroll aşağı indiği zaman navbar'a efekt ekledik.
 function scrollActive(e) {
     if (window.scrollY >= 100) {
-        nav.classList.add("nav__black");
+        nav.classList.add("searchInput");
     } else {
-        nav.classList.remove("nav__black");
+        nav.classList.remove("searchInput");
     }
 }
